802468277dc8c7613ad92da4c8a25742580d35a9
Glassig <angelina.v.gegerfelt@gmail.com>
2022-10-17 23:23:04 +0200

    Sass compile on change

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const http = require("http");
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
+const sass = require("sass");
 
 const host = "localhost";
 const port = 8000;
 
 const app = express();
 app.use(express.static("."));
+
+sass.render(
+  {
+    file: "stylesheets/style.scss",
+  },
+  function (error, result) {
+    if (error) {
+      return;
+    }
+    fs.writeFile("stylesheets/style.css", result.css, () => {});
+  }
+);
 
 app.use("/", function (_req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
